930b9d9125e5596e69f51f22a2bab2645e6458f9
nandkumargangai1@outlook.com <nandkumargangai1@outlook.com>
2020-10-18 22:51:34 +0530

    Added manifest

--- next.config.js
+++ next.config.js
@@ -1,21 +1,22 @@
 const withSass = require('@zeit/next-sass');
-
-const isProd = process.env.NODE_ENV === 'production';
-
+const withOffline = require('next-offline');
 const withPWA = require('next-pwa');
-
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 });
 
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = withSass(
   withMDX(
-    withPWA({
-      pageExtensions: ['js', 'mdx'],
-      pwa: {
-        disable: !isProd,
-        dest: 'public',
-      },
-    })
+    withPWA(
+      withOffline({
+        pageExtensions: ['js', 'mdx'],
+        pwa: {
+          disable: !isProd,
+          dest: 'public',
+        },
+      })
+    )
   )
 );
--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,15 @@ export default function App() {
     <div>
       <Head>
         <title>SpaceX Launch Programs</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='manifest' href='/manifest.json' />
+        <meta name='theme-color' content='#90cdf4' />
+        <link rel='apple-touch-icon' href='/logo-96x96.png' />
+        <meta name='apple-mobile-web-app-status-bar' content='#90cdf4' />
       </Head>
       <main>
         <LandingPage />
       </main>
     </div>
-  )
+  );
 }
