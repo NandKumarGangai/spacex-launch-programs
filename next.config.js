d2a0875affa0306880966fd5db531a12b128739e
nandkumargangai1@outlook.com <nandkumargangai1@outlook.com>
2020-10-18 23:11:30 +0530

    Removed withOffline

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const withSass = require('@zeit/next-sass');
-const withOffline = require('next-offline');
 const withPWA = require('next-pwa');
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
@@ -9,14 +8,12 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withSass(
   withMDX(
-    withPWA(
-      withOffline({
-        pageExtensions: ['js', 'mdx'],
-        pwa: {
-          disable: !isProd,
-          dest: 'public',
-        },
-      })
-    )
+    withPWA({
+      pageExtensions: ['js', 'mdx'],
+      pwa: {
+        disable: !isProd,
+        dest: 'public',
+      },
+    })
   )
 );
